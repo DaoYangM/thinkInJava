d8c9f53538ea93d3c5b777344bb1927125c94db2
DaoYangM <498721777@qq.com>
2018-11-09 17:08:15 +0800

    overloading

---
+++ src/main/java/top/daoyang/initialization/SimpleConstructor.java
@@ -0,0 +1,19 @@
+package top.daoyang.initialization;
+
+class Rock {
+    Rock() {
+        System.out.print("Rock ");
+    }
+
+    Rock(int i) {
+        System.out.print("Rock  " + i + " ");
+    }
+}
+
+public class SimpleConstructor {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            new Rock(i);
+        }
+    }
+}
